98681ef58f72a3228b98e5f4d7d312069a599cfc
Anton <antbl294@student.liu.se>
2023-03-30 21:37:19 +0200

    latest fixä

--- tippatoppen/src/app/list/list.component.ts
+++ tippatoppen/src/app/list/list.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { dragula, DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor() {}
+  ngOnInit(): void {}
+  
   values = ['Value 1', 'Value 2', 'Value 3', 'Value 4', 'Value 5', 'Value 6', 'Value 7', 'Value 8', 'Value 9', 'Value 10'];
 
 }
