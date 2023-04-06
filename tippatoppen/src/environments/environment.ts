7474d3b4db052eed0a088d75baae5a345b71152a
Anton <antbl294@student.liu.se>
2023-04-06 10:51:31 +0200

    old api token example. Needs better handling

--- tippatoppen/src/environments/environment.ts
+++ tippatoppen/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiToken: 'BQApM17LswbduHHpdML12wUNQpLVMegBPTc9e9io6d19rOXA88L9Z2Bh6nKllprPkbQp3hFy3f-_6AnMDLYGbyuJMfotYMgIzAaTxfMGdal8Bx71Py3cELZdPAA3nQS20fLwXXC3I155NFGNSQ5KP7P6TXEbamN2yJJK-S7hhLJ_Ef-UKJO0'
 };
 
 /*
