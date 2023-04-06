faa541f3219d211b09f5935653d17cff662b00b6
Anton <antbl294@student.liu.se>
2023-04-06 14:32:20 +0200

    removed uneccessary import in appmodulets

--- tippatoppen/src/app/app.module.ts
+++ tippatoppen/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { HttpClientModule } from '@angular/common/http';
-import * as Papa from 'papaparse';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 
